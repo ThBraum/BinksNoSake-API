7f90b8ea320c77df60082003ca8d7298fd51a276
Matheus Braum <matheus_braum1@hotmail.com>
2023-12-16 20:42:51 -0300

    DataIngressoTripulacao ToLocalTime

--- BinksNoSake.Domain/Models/PirataModel.cs
+++ BinksNoSake.Domain/Models/PirataModel.cs
@@ -7,7 +7,11 @@ public class PirataModel
     public int Id { get; set; }
     public string? Nome { get; set; }
     public string? Funcao { get; set; }
-    public DateTime? DataIngressoTripulacao { get; set; }
+    public DateTime? DataIngressoTripulacao 
+    {
+        get => _dataIngressoTripulacao;
+        set => _dataIngressoTripulacao = value.HasValue ? value.Value.ToLocalTime() : null;
+    }
     public string? Objetivo { get; set; }
     public string? ImagemURL { get; set; }
 
@@ -17,4 +21,5 @@ public class PirataModel
 
     // Relação com Navios
     public IEnumerable<NavioModel>? Navios { get; set; }
+    private DateTime? _dataIngressoTripulacao;
 }
